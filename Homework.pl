0fb3dcf81c2f04981769494fe6ede70df8a1653b
Moraru Teodor Valentin <38580519+MoraruValentin@users.noreply.github.com>
2019-04-04 21:43:48 +0300

    Add files via upload

--- Homework.pl
+++ Homework.pl
@@ -11,14 +11,14 @@ route(A, B) :- direct_train(A, B), !.
 route(A, B) :- direct_train(A, X), route(X, B).
 
 /**
- *route(+Location_A : A,+Location_B : B) :- direct_train(+Location_A : A, +Location_B : B), !.
+ *route(+Location_A : variable, +Location_B : variable).
  *route is True if there is a direct train between locations A and B.
  *So the base case or the stop case is as following: if there is a direct train between A and B we have a route between A and B.
  *! operator is used so the program will not backtrack back behind this operator.
  *
- *route(+Location_A : A,+Location_B : B) :- direct_train(+Location_A : A, ?Location_X : X), route(?Location_X : X, +Location_B : B).
  *route is true if there is a direct train between A and another location X and also a route between X and the final destination B.
  *This represents the recursive case because the route(X,B) will follow the same arboration style as the initial route(A,B) going to the base case situation.
+ *
  *
  *Examples:
  *
@@ -49,12 +49,10 @@ means([Head|Tail], [ResHead|ResTail]):-
              means(Tail,ResTail).			
 
 /**
- *means(?List : [],?List : []).
  *The base or the stop case for this recursion represents two empty lists which means we have emptied out the list of words to be translated.
  *
- *means(+List : [Head|Tail], -Res_List : [ResHead|ResTail]) :-
- *translate(+List_Head : Head, -Res_List_Head : ResHead),
- *means(+List_Tail : Tail, -Res_List_Tail : ResTail).
+ *means(+List_in : list, -List_result : list).
+ *
  *The recursion will go as follows: the head of the input list will be translated with the avalaible predicate if it exists.
  *means will recall itself on the Tail of the remaining list untill the base case which is an empty list so all elements were evaluated.
  *if there is a single element that cannot be translated, there is no predicate translate declare for it, the function will return false.
@@ -74,20 +72,84 @@ means([Head|Tail], [ResHead|ResTail]):-
 
 %Problema III     
 append([], List, List).
-append([Head|Tail], List, [Head|Rest]) :- append(Tail, List, Rest).
+append([Head|Tail], List, [Head|Rest]) :- 
+	append(Tail, List, Rest).
 
 duplicate_El(Elem, 1, [Elem]).
-duplicate_El(Elem, N, [Elem|DupedEl]) :- N > 1, N2 is N-1, duplicate_El(Elem, N2, DupedEl). 
+duplicate_El(Elem, N, [Elem|DupedEl]) :- 
+	N > 1, N2 is N-1, 
+	duplicate_El(Elem, N2, DupedEl). 
 
-duplicate_N([], _, []).
-duplicate_N([Head|Tail], N, DupedList) :- duplicate_El(Head, N, DupedH), duplicate_N(Tail, N, DupedT), append(DupedH, DupedT, DupedList). 
+duplicate_NList([], _, []).
+duplicate_NList([Head|Tail], N, DupedList) :- 
+	duplicate_El(Head, N, DupedH), 
+	duplicate_NList(Tail, N, DupedT), 
+	append(DupedH, DupedT, DupedList). 
 
+/**
+ *append(+List1 : list, +List2 : list, +List3: list).
+ *This is an utilitary funtion used to append two lists.
+ *
+ *duplicate_El(+Elem : variable, +N : Integer, -List : list).
+ *This is an utilitary recursive function used to duplicate an element N times, the result is stored in a list.
+ *The base case happens when the N iterator is decremented to 1, 
+ *then recursively the end list is formed by adding an aditional Element for every iteration that happened.
+ *
+ *duplicate_NList(+List : list, +N : Integer, -DupedList : list).
+ *This is the function used to duplicate every element from List N times and adding the resulting data into DupedList.
+ *The base case happens when the list Head is empty ( [] ), after every iteration the function is recalled on Tail, after this is empty we go to stop case.
+ *After this at the revursive return every Duped Tail is Appended to its DupedHead, a DupedHead takes the place of DupedTail for its DupedHead predecessor untill the first DupedHead.
+ *The end result will be a list will all elements from the input list duped N times.
+ *
+ *The only fail we can encounter happens when we try tu duplicate something 0 times, then the interogation will return false.
+ *
+ *Examples:
+ *
+ *?-append([1,2,3],[4,5,6],Res)
+ *Res = [1, 2, 3, 4, 5, 6]
+ *
+ *?-duplicate_El(x,4,Res)
+ *Res = [x, x, x, x]
+ *
+ *?-duplicate_NList([x,y,z],3,Res)
+ *Res = [x, x, x, y, y, y, z, z, z]
+ *
+ *?duplicate_NList([x,y,z],0,Res)
+ *false
+ */
 
 %Problema IV
-insert_at(Elem, 1, Tail, [Elem|Tail]).  %stop case add elem to tail as head of new list then append the rest recursively back.
+insert_at(Elem, 1, Tail, [Elem|Tail]).
 insert_at(Elem, N, [Head|Tail], [Head|Res_tail]) :-
         N1 is N - 1,
         insert_at(Elem, N1, Tail, Res_tail).
+
+/**
+ *insert_at(+Elem : variable, +Position : Integer, ?List1 : list, ?List2 : list).
+ *
+ *base case or stop case happens when the Position decrements to 1, this means the element can pe appended to the tail of the list is his deisred position.
+ *after the base case the program will exit recursively adding behind the new tail ( [Element|Tail] ) the initial Head of list thus forming the new list with the Element in the desired position.
+ *
+ *The only fails we can encounter will happen of we try to add outside of the bounds of our list size.
+ *
+ *Examples:
+ *
+ *?-insert_at(x,3,[1,2,3,4,5],Res)
+ *Res = [1, 2, x, 3, 4, 5]
+ *
+ *?-insert_at(x,1,[1,2,3,4,5],Res)
+ *Res = [x, 1, 2, 3, 4, 5]
+ *
+ *?-insert_at(x,6,[1,2,3,4,5],Res)
+ *Res = [1, 2, 3, 4, 5, x]
+ *
+ *?-insert_at(x,0,[1,2,3,4,5],Res)
+ *false
+ *
+ *?-insert_at(x,7,[1,2,3,4,5],Res)
+ *false
+ * 
+ */
 
 
 %Problema V
@@ -98,14 +160,12 @@ range(Low,High,[Low|List_tail]) :-
     New_Low is Low+1,				
     range(New_Low,High,List_tail).	
 /**
- *range(+Low_value : Same, +High_value : Same, -List_head: [Same]) :- !.
  *The base or the stop case for the recursion happens when Low == High == Head so the conting has ended.
  *! operator is used so the program will not backtrack back behind this operator.
  *
- *range(+Low_value : Low, +High_value : High, -List : [Low|List_tail]) :-
- *Low =< High,
- *New_Low is Low+1,  (New_low is Low+1 in order to call the recursion for the rest of the counting)
- *range(?New_low_value : New_Low, +High_value : High, -List_tail : List_tail).
+ *range(+Low : variable, +High : variable, -List : list).
+ *Low and High will be the bounds of the range we want to create.
+ *
  *The recursion will go as follows : while Low =< High the new low value will be Low + 1, note that if Low > High the function will fail and return false.
  *the function range will recall itself of (New_Low,High,List_tail) untill New_Low and High have the same value, see abose stop case.
  *after the stop case the Result will form recursively the result list, ranging from every New_Low to the initial Low value.
